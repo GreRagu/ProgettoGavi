07aa2dae903e7b756dd77aac52c1ca7da7d46830
Andre96H <helmiandrea96@gmail.com>
2019-01-16 15:30:44 +0100

    Creazione della grafica

--- src/ProgettoGaviMain.java
+++ src/ProgettoGaviMain.java
@@ -4,10 +4,21 @@ import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
 
 public class ProgettoGaviMain {
 
-	private JFrame frame;
+	private JFrame frmHegregio;
+	private JTextField txtSearch;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -17,7 +28,7 @@ public class ProgettoGaviMain {
 			public void run() {
 				try {
 					ProgettoGaviMain window = new ProgettoGaviMain();
-					window.frame.setVisible(true);
+					window.frmHegregio.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,12 +47,13 @@ public class ProgettoGaviMain {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmHegregio = new JFrame();
+		frmHegregio.setTitle("Hegregio");
+		frmHegregio.setBounds(100, 100, 450, 300);
+		frmHegregio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
+		frmHegregio.setJMenuBar(menuBar);
 		
 		JMenu mnIndexpath = new JMenu("IndexPath");
 		menuBar.add(mnIndexpath);
@@ -69,6 +81,25 @@ public class ProgettoGaviMain {
 		
 		JMenuItem mntmCalculateAndPlot = new JMenuItem("Calculate and plot");
 		mnEfficiency.add(mntmCalculateAndPlot);
+		frmHegregio.getContentPane().setLayout(null);
+		
+		JButton btnSearch = new JButton("search");
+		btnSearch.setBounds(89, 11, 57, 21);
+		frmHegregio.getContentPane().add(btnSearch);
+		
+		txtSearch = new JTextField();
+		txtSearch.setText("Inserire testo da cercare");
+		txtSearch.setBounds(156, 12, 206, 20);
+		frmHegregio.getContentPane().add(txtSearch);
+		txtSearch.setColumns(15);
+		
+		table = new JTable();
+		table.setBounds(62, 79, 300, 150);
+		frmHegregio.getContentPane().add(table);
+		
+		JLabel lblRicercaSuN = new JLabel("Ricerca su N file con modello");
+		lblRicercaSuN.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRicercaSuN.setBounds(61, 54, 301, 14);
+		frmHegregio.getContentPane().add(lblRicercaSuN);
 	}
-
 }
