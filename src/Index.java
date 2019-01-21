d4054dc645f72fb7a5e545c77df1c9ee8fbbf70f
GiovanniGambiglianiZoccoli <giovanni.gambigliani@outlook.com>
2019-01-21 11:59:56 +0100

    index

--- src/Index.java
+++ src/Index.java
@@ -5,7 +5,6 @@ import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.demo.IndexFiles;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
@@ -13,40 +12,45 @@ import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-import org.apache.lucene.util.Version;
 
 
 
 public class Index {
+	
+	public static final String CONTENTS = "contents";
+	public static final String FILE_NAME = "filename";
+	public static final String FILE_PATH = "filepath";
+	public static final int MAX_SEARCH = 10;
 
-	public static void Main() {
+	public static void main(String[] args) throws IOException {
+		Index i = new Index();
+	}
+	
+	public Index() throws IOException {
 		File toIndexFile = new File("../dataset/clinical_dataset/pmc-text-00/01/2668905.nxml");
 		IndexFile(toIndexFile);
 	}
 	
-	   public static final String CONTENTS = "contents";
-	   public static final String FILE_NAME = "filename";
-	   public static final String FILE_PATH = "filepath";
-	   public static final int MAX_SEARCH = 10;
-	
 	private Document getDocument(File file) throws IOException {
-		   Document document = new Document();
-		   
-		   //index file contents
-		   TextField contentField = new TextField(CONTENTS, "TESTO DA RICERCARE", Field.Store.YES);
-		   
-		   //index file name
-		   TextField fileNameField = new TextField(FILE_NAME, file.getName(), Field.Store.NO);
-		   
-		   //index file path
-		   TextField filePathField = new TextField(FILE_PATH, file.getCanonicalPath(), Field.Store.NO);
-
-		   document.add(contentField);
-		   document.add(fileNameField);
-		   document.add(filePathField);
-
-		   return document;
-		}
+		System.out.println("Indexing "+ file.getCanonicalPath());
+	   Document document = new Document();
+	   
+	   //index file contents
+	   TextField contentField = new TextField(CONTENTS, "TESTO DA RICERCARE", Field.Store.YES);
+	   
+	   //index file name
+	   TextField fileNameField = new TextField(FILE_NAME, file.getName(), Field.Store.NO);
+	   
+	   //index file path
+	   TextField filePathField = new TextField(FILE_PATH, file.getCanonicalPath(), Field.Store.NO);
+	
+	   System.out.println(contentField + " " + fileNameField + " " + filePathField);
+	   document.add(contentField);
+	   document.add(fileNameField);
+	   document.add(filePathField);
+	
+	   return document;
+	}
 	
 	private IndexWriter writer;
 
@@ -61,15 +65,11 @@ public class Index {
 	   writer =new IndexWriter(	indexDirectory, iwc);
 	}
 	
-	private void indexFile(File file) throws IOException {
-		   System.out.println("Indexing "+file.getCanonicalPath());
-		   Document document = getDocument(file);
-		   writer.addDocument(document);
-		   
-		}
-	
-	
-	
-	
-	
+	private void IndexFile(File file) throws IOException {
+	   System.out.println("Indexing "+ file.getCanonicalPath());
+	   
+	   Document document = getDocument(file);
+	   writer.addDocument(document);
+	   
+	}
 }
